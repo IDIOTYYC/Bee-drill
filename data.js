3ca1f7d20d2b18167cbea72308048ab062766235
Shane Fast <shane@get-paper.com>
2018-01-22 02:38:42 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 module.exports = {
     url: '<insert your login page here>',
     credentials: {
-        "emailOne": '"(function(){var date = new Date(); do{curDate = new Date();}while(curDate-date<10000); return Math.max();})()"', //DoS NoSQL injection string
-        "emailOnePass": '"(function(){var date = new Date(); do{curDate = new Date();}while(curDate-date<10000); return Math.max();})()"',
+        "emailOne": '"$where: (function(){var date = new Date(); do{curDate = new Date();}while(curDate-date<10000); return Math.max();})()"', //DoS NoSQL injection string
+        "emailOnePass": '"$where: (function(){var date = new Date(); do{curDate = new Date();}while(curDate-date<10000); return Math.max();})()"',
     },
     test_one: {
         exp_title: '<expected error return>'
